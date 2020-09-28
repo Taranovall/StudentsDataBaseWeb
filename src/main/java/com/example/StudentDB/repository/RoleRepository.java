7ebd5c6473968656319f539f29846cec6e4bd814
Taranovall <taranov5204@gmail.com>
2020-09-28 22:54:22 +0300

    .

--- src/main/java/com/example/StudentDB/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.StudentDB.repository;
-
-import com.example.StudentDB.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role,Long> {
-}
--- src/main/java/com/example/StudentDB/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.StudentDB.repository;
-
-import com.example.StudentDB.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-    User findByUsername(String username);
-}
